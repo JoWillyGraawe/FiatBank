b08ad05fffdf253d2e022df0996bd9d2b48e586e
VegardJohnsen <91907821+VegardJohnsen@users.noreply.github.com>
2021-11-09 19:07:17 +0100

    Update loan_calculator.js

--- website/script/loan_calculator.js
+++ website/script/loan_calculator.js
@@ -1,1 +1,11 @@
+document.getElementById("btn").onclick = function(){
+    var x = Math.round(document.getElementById("income").value * 5);
+    var y = Math.round(document.getElementById("equity").value * (85/15));
+    if (x > y){
+        document.getElementById("total").value = y;
+    }
+    else{
+        document.getElementById("total").value = x;
+    }
+}
 
