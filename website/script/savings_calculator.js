f5744d9293b164fdcebeedd1f4556ee5f3ad5eb5
SanderPils <91139908+SanderPils@users.noreply.github.com>
2021-11-09 19:52:51 +0100

    Update savings_calculator.js

--- website/script/savings_calculator.js
+++ website/script/savings_calculator.js
@@ -1,1 +1,7 @@
-
+function savingsCalculation(){
+    let initialValue = document.getElementById("Initial").value;
+    let interestValue = document.getElementById("Interest").value;
+    let timeValue = document.getElementById("Time").value;
+    let FV = initialValue*((1+(interestValue/100)) ** timeValue);
+    document.getElementById("FV").value = FV.toFixed();
+  }
