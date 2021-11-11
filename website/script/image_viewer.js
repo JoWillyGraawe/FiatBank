34497f59d24e6887e009163ff676b653a3caed95
Jo Willy Gråwe <jowilly@stud.ntnu.no>
2021-11-11 18:01:25 +0100

    Added image_viewer.js

--- website/script/image_viewer.js
+++ website/script/image_viewer.js
@@ -1,1 +1,12 @@
+// Function for enlarging image
+function viewImageToggle(img) {
+    if (img.classList.contains("zoomed")) {
+        img.style.transform = "scale(1)";
+        img.style.transition = "transform 0.25s ease";
+    } else {
+        img.style.transform = "scale(2.0)";
+        img.style.transition = "transform 0.25s ease";
+    };
+    img.classList.toggle("zoomed");
+};
 
