a80a78341b26e66520db3f3b1270bc9401f1c470
iamvalentinst <reliotin@gmail.com>
2021-11-11 16:22:37 +0100

    something

---
+++ modularity/script/main.js
@@ -0,0 +1,4 @@
+/*
+This is our main JS. Here we will import every other js and combine them in on js file (main.js).
+We decided it would be easier to just have every function in one file. But the function  
+*/
